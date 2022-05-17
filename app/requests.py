1030949bc2ed1449223cc013e53238a0623dc5d3
Mathenge-Alex <arxmaina@gmail.com>
2022-05-17 14:30:29 +0300

    initial folder structure

---
+++ app/requests.py
@@ -0,0 +1,15 @@
+
+import urllib.request,json
+# import requests 
+
+# Getting api key
+api_key = None
+
+
+def configure_request(app):
+    global api_key
+    # api_key = app.config['API_KEY']
+
+
+    
+    
