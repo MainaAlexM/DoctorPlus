1a98cd1d8561075ab2e3d4a94c7f2cd6839317e1
Mathenge-Alex <arxmaina@gmail.com>
2022-05-20 06:48:04 +0300

    Prod: Include app database

--- app/main/error.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template
-from . import main
-
-@main.app_errorhandler(404)
-def errors(error):
-    '''
-    Function to render the 404 error page
-    '''
-    return render_template('main/errors.html'),404
