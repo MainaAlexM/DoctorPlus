d2016652e901675f6d26baedc0747eb37bb8a792
Mathenge-Alex <arxmaina@gmail.com>
2022-05-20 05:30:51 +0300

    Perf: Initialize database

--- app.py
+++ app.py
@@ -11,6 +11,31 @@ from flask_mail import *
 
 app = Flask(__name__)
 
+mail = Mail(app)
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'apptestermind@gmail.com'
+app.config['MAIL_PASSWORD'] = 'Apptester20#'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
+mail = Mail(app)
+
+app.secret_key = 'secrete key'
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///employer.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hosp.db'
+app.config['SECRET_KEY'] = '0527'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['TESTING'] = False
+
+db = SQLAlchemy(app)
+#ap = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app )
+login_manager.login_view = 'login'
+
 
    
 
